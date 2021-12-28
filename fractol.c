6c251e19aeb6302efbe1e310c881e4b527c80891
Nuno <nlouro@student.42heilbronn.de>
2021-12-28 22:44:39 +0100

    Two pixels showing color using mlx_pixel_put

--- fractol.c
+++ fractol.c
@@ -6,32 +6,33 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 15:12:55 by nlouro            #+#    #+#             */
-/*   Updated: 2021/12/28 17:13:42 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/12/28 22:42:55 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
 
-void	my_mlx_pixel_put(Fractal *data, int x, int y, int color)
+void	my_mlx_pixel_put(Fractal *fr, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = fr->addr + (y * fr->line_length + x * (fr->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
 
+/*
 void	mandelbrot(Fractal *f)
 {
 	printf("Hello\n");
-	mlx_pixel_put(f->mlx, f->window, 0, 0, 1);
-	//mlx_pixel_put(f.mlx, f.window, 0, 1, 1);
+	//mlx_pixel_put(f->mlx, f->window, 0, 0, 1);
 }
+*/
 
-int	handle_key(int keycode, Fractal f)
+int	handle_key(int keycode, Fractal *f)
 {
-	printf("set: %s", f.set);
 	printf("key pressed %i", keycode);
+	printf("set: %s", f->set);
 	return (0);
 }
 
@@ -42,6 +43,8 @@ int	handle_key(int keycode, Fractal f)
 
 int	main(int argc, char **argv)
 {
+    void    *mlx;
+    void    *window;
 	Fractal fr;
 	//int size_x;
 	//int size_y;
@@ -60,14 +63,20 @@ int	main(int argc, char **argv)
 	fr.size_x = 400;
 	fr.size_y = 400;
 
-	//printf("Hello\n");
-	fr.mlx = mlx_init();
-	fr.window = mlx_new_window(fr.mlx, fr.size_x, fr.size_y, "Fract-ol");
-	fr.image = mlx_new_image(fr.mlx, fr.size_x, fr.size_y);
-	fr.addr = mlx_get_data_addr(fr.image, &fr.bits_per_pixel, &fr.line_length, &fr.endian);
-	my_mlx_pixel_put(&fr, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(fr.mlx, fr.window, fr.image, 0, 0);
-	mlx_key_hook(fr.window, handle_key, &fr);
-	mlx_loop(fr.mlx);
+	mlx = mlx_init();
+	window = mlx_new_window(mlx, fr.size_x, fr.size_y, "Fract-ol");
+
+	mlx_pixel_put(mlx, window, 5, 5, 0x00FF0000);
+	mlx_pixel_put(mlx, window, 5, 15, 0x00FFFFFF);
+	//fr.image = mlx_new_image(fr.mlx, fr.size_x, fr.size_y);
+	//fr.addr = mlx_get_data_addr(fr.image, &fr.bits_per_pixel, &fr.line_length, &fr.endian);
+	//my_mlx_pixel_put(&fr, 5, 5, 0x00FF0000);
+
+	//mlx_put_image_to_window(fr.mlx, fr.window, fr.image, 0, 0);
+	//mlx_put_image_to_window(mlx, window, fr.image, 0, 0);
+	//mlx_key_hook(fr.window, handle_key, &fr);
+	mlx_key_hook(window, handle_key, &fr);
+
+	mlx_loop(mlx);
 	return (0);
 }
--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 15:16:37 by nlouro            #+#    #+#             */
-/*   Updated: 2021/12/28 17:26:14 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/12/28 17:29:13 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 
 typedef struct Fractals 
 {
-	void	*mlx;
-	void	*window;
+	//void	*mlx;
+	//void	*window;
 
 	/* https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html */
 	void	*image;
