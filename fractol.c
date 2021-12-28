6e449171d20dada4c528dffbb6f4e8b0e2dc5c1f
Nuno <nlouro@student.42heilbronn.de>
2021-12-28 17:27:38 +0100

    window opening but events not caught and unable to draw in window

--- fractol.c
+++ fractol.c
@@ -6,24 +6,68 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 15:12:55 by nlouro            #+#    #+#             */
-/*   Updated: 2021/12/27 16:24:40 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/12/28 17:13:42 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	main(void)
+
+void	my_mlx_pixel_put(Fractal *data, int x, int y, int color)
 {
-	void *mlx_ptr;
-	int size_x;
-	int size_y;
-	char *title = "Fract-ol";
+	char	*dst;
 
-	size_x = 1024;
-	size_y = 768;
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
+void	mandelbrot(Fractal *f)
+{
 	printf("Hello\n");
-	mlx_ptr = mlx_init();
-	mlx_new_window(mlx_ptr, size_x, size_y, title);
+	mlx_pixel_put(f->mlx, f->window, 0, 0, 1);
+	//mlx_pixel_put(f.mlx, f.window, 0, 1, 1);
+}
+
+int	handle_key(int keycode, Fractal f)
+{
+	printf("set: %s", f.set);
+	printf("key pressed %i", keycode);
+	return (0);
+}
+
+/*void	close_and_exit(s)
+{
+	mlx_destroy_window(s.mlx, s.window);
+}*/
+
+int	main(int argc, char **argv)
+{
+	Fractal fr;
+	//int size_x;
+	//int size_y;
+
+	if (argc < 2)
+	{
+		printf("ERROR: wrong args. Call as:\n fractol <fractal set> <params>\n"); 
+		return (1);
+	}
+	if (strcmp(argv[1], "Julia") == 0)
+		printf("Julia set");
+	else if (strcmp(argv[1], "Mandelbrot") == 0)
+		printf("Mandelbrot set");
+	fr.set = argv[1];
+
+	fr.size_x = 400;
+	fr.size_y = 400;
+
+	//printf("Hello\n");
+	fr.mlx = mlx_init();
+	fr.window = mlx_new_window(fr.mlx, fr.size_x, fr.size_y, "Fract-ol");
+	fr.image = mlx_new_image(fr.mlx, fr.size_x, fr.size_y);
+	fr.addr = mlx_get_data_addr(fr.image, &fr.bits_per_pixel, &fr.line_length, &fr.endian);
+	my_mlx_pixel_put(&fr, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(fr.mlx, fr.window, fr.image, 0, 0);
+	mlx_key_hook(fr.window, handle_key, &fr);
+	mlx_loop(fr.mlx);
 	return (0);
 }
--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 15:16:37 by nlouro            #+#    #+#             */
-/*   Updated: 2021/12/27 15:20:14 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/12/28 17:26:14 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,28 @@
 # define FRACTOL_H
 
 # include <unistd.h>
+# include <stdlib.h> //used for exit
 # include "mlx.h"
 
-# include <stdio.h>
+typedef struct Fractals 
+{
+	void	*mlx;
+	void	*window;
+
+	/* https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html */
+	void	*image;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+
+	char	*set;
+	int		size_x;
+	int		size_y;
+	//int		ymin
+}	Fractal;
+
+# include <stdio.h> //used for printf
+# include <string.h> //used for strcmp 
 
 #endif
